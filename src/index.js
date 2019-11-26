53bdecbab0af16f9c23a287c303b6c31eb08b3c5
Maksym <wolfsangel009@gmail.com>
2019-11-27 00:44:22 +0200

    Edited structure of project, integrated React into the project. Finished my mini application.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-
+import { Provider } from 'react-redux';
 import reducer from './reducer';
-
-import { inc, dec, rnd } from './actions';
+import App from './components/app';
 
 const store = createStore(reducer);
 
-document
-   .getElementById('inc')
-   .addEventListener('click', () => {
-      store.dispatch(inc());
-   });
-
-document
-   .getElementById('dec')
-   .addEventListener('click', () => {
-      store.dispatch(dec());
-   });
-
-document
-   .getElementById('rnd')
-   .addEventListener('click', () => {
-      const payload = Math.floor(Math.random()*10);
-      store.dispatch(rnd(payload));
-   });
-
-const update = () => {
-   document
-      .getElementById('counter')
-      .innerHTML = store.getState();
-};
-
-store.subscribe(update);
+   ReactDOM.render(
+      <Provider store={store}>
+         <App />
+      </Provider>,
+      document.getElementById('root'));
